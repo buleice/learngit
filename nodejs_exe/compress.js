45a8eb246f7a215c9afd8faf535e1de0df6754c9
Alexander <1920343977@qq.com>
2018-07-11 16:03:43 +0800

    nodejs练习

---
+++ nodejs_exe/compress.js
@@ -0,0 +1,4 @@
+let fs=require('fs');
+let zlib=require('zlib');
+fs.createReadStream('input.txt').pipe(zlib.createGzip()).pipe(fs.createWriteStream('input.txt.gz'));
+console.log('over')
---
+++ nodejs_exe/test.js
@@ -0,0 +1,53 @@
+events 模块只提供了一个对象： events.EventEmitter。EventEmitter 的核心就是事件触发与事件监听器功能的封装。
+
+你可以通过require("events");来访问该模块。
+
+// 引入 events 模块
+var events = require('events');
+// 创建 eventEmitter 对象
+var eventEmitter = new events.EventEmitter();
+EventEmitter 对象如果在实例化时发生错误，会触发 error 事件。当添加新的监听器时，newListener 事件会触发，当监听器被移除时，removeListener 事件被触发。
+
+下面我们用一个简单的例子说明 EventEmitter 的用法：
+
+//event.js 文件
+var EventEmitter = require('events').EventEmitter; 
+var event = new EventEmitter(); 
+event.on('some_event', function() { 
+    console.log('some_event 事件触发'); 
+}); 
+setTimeout(function() { 
+    event.emit('some_event'); 
+}, 1000); 
+执行结果如下：
+
+运行这段代码，1 秒后控制台输出了 'some_event 事件触发'。其原理是 event 对象注册了事件 some_event 的一个监听器，然后我们通过 setTimeout 在 1000 毫秒以后向 event 对象发送事件 some_event，此时会调用some_event 的监听器。
+
+$ node event.js 
+some_event 事件触发
+EventEmitter 的每个事件由一个事件名和若干个参数组成，事件名是一个字符串，通常表达一定的语义。对于每个事件，EventEmitter 支持 若干个事件监听器。
+
+当事件触发时，注册到这个事件的事件监听器被依次调用，事件参数作为回调函数参数传递。
+
+让我们以下面的例子解释这个过程：
+
+//event.js 文件
+var events = require('events'); 
+var emitter = new events.EventEmitter(); 
+emitter.on('someEvent', function(arg1, arg2) { 
+    console.log('listener1', arg1, arg2); 
+}); 
+emitter.on('someEvent', function(arg1, arg2) { 
+    console.log('listener2', arg1, arg2); 
+}); 
+emitter.emit('someEvent', 'arg1 参数', 'arg2 参数'); 
+执行以上代码，运行的结果如下：
+
+$ node event.js 
+listener1 arg1 参数 arg2 参数
+listener2 arg1 参数 arg2 参数
+以上例子中，emitter 为事件 someEvent 注册了两个事件监听器，然后触发了 someEvent 事件。
+
+运行结果中可以看到两个事件监听器回调函数被先后调用。 这就是EventEmitter最简单的用法。
+
+EventEmitter 提供了多个属性，如 on 和 emit。on 函数用于绑定事件函数，emit 属性用于触发一个事件。接下来我们来具体看下 EventEmitter 的属性介绍。
